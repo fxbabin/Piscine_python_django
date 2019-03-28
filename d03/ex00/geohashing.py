bd3351ec30fd33a61dc8f482b7366a8d80ba6513
fxbabin <fxbabin@outlook.fr>
2019-03-28 10:15:05 +0100

    starting d03

--- d03/ex00/geohashing.py
+++ d03/ex00/geohashing.py
@@ -1,9 +1,27 @@
 #! /usr/bin/python3
 
-import sys, antigravity
+import sys
+from antigravity import geohash
 
 def main():
-    print('tti')
+    if (len(sys.argv) != 5):
+        print ("Error : wrong number of arguments for geohashing.\n"
+               "waiting following arguments:\n"
+               "- latitude (ex: 37.421542)\n"
+               "- longitude (ex: -122.085589)\n"
+               "- date (ex : 2005-05-26)\n"
+               "- dow (ex: 10458.68)\n")
+        return
+    try:
+        latitude = float(sys.argv[1])
+        longitude = float(sys.argv[2])
+        date = sys.argv[3]
+        dow = float(sys.argv[4])
+    except ValueError:
+        print("Error : wrong type for latitude, longitude or dow")
+        return
+    tmp = "{}-{}".format(date, dow).encode('ascii')
+    geohash(latitude, longitude, tmp)
 
 if __name__ == '__main__':
     main()
---
+++ d03/ex01/my_script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip --version
+git clone https://github.com/jaraco/path.py.git
