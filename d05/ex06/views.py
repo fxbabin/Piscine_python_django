d9b1bffd4a641331e3117d36c606306127379bd9
Francois-xavier BABIN <fbabin@e3r12p17.42.fr>
2019-04-02 00:35:10 +0200

    coninuing ex06

--- d05/ex06/views.py
+++ d05/ex06/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+def init(request):
+    try:
+        conn = psycopg2.connect(
+            database='formationdjango',
+            host='localhost',
+            user='djangouser',
+            password='secret'
+        )    
+
+        curr = conn.cursor()
+
+        curr.execute(""" 
+        CREATE TABLE IF NOT EXISTS ex06_movies (
+            title           varchar(64)     NOT NULL UNIQUE,
+            episode_nb      integer         PRIMARY KEY,
+            opening_crawl   text,
+            director        varchar(32)     NOT NULL,
+            producer        varchar(128)    NOT NULL,
+            release_date    date            NOT NULL
+        )
+        """)
+        conn.commit()
+        conn.close()
+        return HttpResponse('OK')
+    except psycopg2.Error as e:
+        return HttpResponse(e)
+    return HttpResponse('you should not be here!')
